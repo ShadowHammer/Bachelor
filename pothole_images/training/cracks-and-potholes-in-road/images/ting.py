2f0c64adc0b860879812405b7e4fd96e5605c855
ShadowHammer <63354418+ShadowHammer@users.noreply.github.com>
2024-03-04 14:04:45 +0100

    aps

--- pothole_images/training/cracks-and-potholes-in-road/images/ting.py
+++
@@ -1,7 +0,0 @@
-import os, csv
-
-f=open("C:/Users/tobia/Documents/GitHub/Bachlor/pothole_images/training/cracks-and-potholes-in-road/images/filer.csv",'r+')
-w=csv.writer(f)
-for path, dirs, files in os.walk("C:/Users/tobia/Documents/GitHub/Bachlor/pothole_images/training/cracks-and-potholes-in-road/images"):
-    for filename in files:
-        w.writerow([filename])
