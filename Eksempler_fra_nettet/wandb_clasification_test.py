413f3cac7ec6040bcae48df19ea14624d7eabb09
ShadowHammer <63354418+ShadowHammer@users.noreply.github.com>
2024-03-08 11:16:31 +0100

    wandb works

--- Eksempler_fra_nettet/wandb_clasification_test.py
+++ Eksempler_fra_nettet/wandb_clasification_test.py
@@ -36,4 +36,154 @@ class CIFAR10DataModule(pl.LightningDataModule):
     def setup(self, stage = None):
         # Assign train/val datasets for use in dataloaders
         if stage == 'fit' or stage is None:
-            
+            cifar_full = CIFAR10(self.data_dir, train=True, transform = self.transform)
+            self.cifar_train, self.cifar_val = random_split(cifar_full, [45000, 5000])
+
+        # Assign test dataset for use in dataloader(s)
+        if stage == 'test' or stage is None:
+            self.cifar_test = CIFAR10(self.data_dir, train=False, transform = self.transform)
+    
+    def train_dataloader(self):
+        return DataLoader(self.cifar_train, batch_size=self.batch_size, shuffle=True)
+    
+    def val_dataloader(self):
+        return DataLoader(self.cifar_val, batch_size=self.batch_size)
+    
+    def test_dataloader(self):
+        return DataLoader(self.cifar_test, batch_size=self.batch_size)
+    
+
+class ImagePredictionLogger(pl.callbacks.Callback):
+    def __init__(self, val_samples, num_samples=32):
+        super().__init__()
+        self.num_samples = num_samples
+        self.val_imgs, self.val_labels = val_samples
+        
+    def on_validation_epoch_end(self, trainer, pl_module):
+        val_imgs = self.val_imgs.to(device=pl_module.device)
+
+        logits = pl_module(val_imgs)
+        preds = torch.argmax(logits, -1)
+
+        trainer.logger.experiment.log({
+            "examples":[wandb.Image(x, caption=f"Pred:{pred}, Target:{y}") for x, pred, y in zip(val_imgs[:self.num_samples], preds, self.val_labels[:self.num_samples])]
+        })
+    
+class LitModel(pl.LightningModule):
+    def __init__(self, input_shape, num_classes, learning_rate=2e-4):
+        super().__init__()
+        # log hyperparameters
+        self.save_hyperparameters()
+        self.learning_rate = learning_rate
+        
+        self.conv1 = nn.Conv2d(3, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 32, 3, 1)
+        self.conv3 = nn.Conv2d(32, 64, 3, 1)
+        self.conv4 = nn.Conv2d(64, 64, 3, 1)
+
+        self.pool1 = torch.nn.MaxPool2d(2)
+        self.pool2 = torch.nn.MaxPool2d(2)
+        
+        n_sizes = self._get_conv_output(input_shape)
+
+        self.fc1 = nn.Linear(n_sizes, 512)
+        self.fc2 = nn.Linear(512, 128)
+        self.fc3 = nn.Linear(128, num_classes)
+        # Accuracy needs an argument tesk
+
+        self.accuracy = Accuracy(task = "multiclass",num_classes=num_classes)
+
+    def _get_conv_output(self, shape):
+        batch_size = 1
+        input = torch.autograd.Variable(torch.rand(batch_size, *shape))
+        output_feat = self._forward_features(input)
+        n_size = output_feat.data.view(batch_size, -1).size(1)
+        return n_size
+
+
+    def forward(self, x):
+        x = self._forward_features(x)
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.log_softmax(self.fc3(x), dim=1)
+        
+        return x
+    
+    def _forward_features(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.pool1(F.relu(self.conv2(x)))
+        x = F.relu(self.conv3(x))
+        x = self.pool2(F.relu(self.conv4(x)))
+        return x
+    
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = F.nll_loss(logits, y)
+        preds = torch.argmax(logits, dim=1)
+        acc = self.accuracy(preds, y)
+        self.log('train_loss', loss, on_step=True, on_epoch=True, logger=True)
+        self.log('train_acc', acc, on_step=True, on_epoch=True, logger=True)
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = F.nll_loss(logits, y)
+        preds = torch.argmax(logits, dim=1)
+        acc = self.accuracy(preds, y)
+        self.log('val_loss', loss, prog_bar=True)
+        self.log('val_acc', acc, prog_bar=True)
+        return loss
+    
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = F.nll_loss(logits, y)
+        preds = torch.argmax(logits, dim=1)
+        acc = self.accuracy(preds, y)
+        self.log('test_loss', loss, prog_bar=True)
+        self.log('test_acc', acc, prog_bar=True)
+        return loss
+    
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+        return optimizer
+        
+dm = CIFAR10DataModule(batch_size=32)
+# To access the x_dataloader we need to call prepare_data and setup.
+dm.prepare_data()
+dm.setup()
+
+# Samples required by the custom ImagePredictionLogger callback to log image predictions.
+val_samples = next(iter(dm.val_dataloader()))
+val_imgs, val_labels = val_samples[0], val_samples[1]
+val_imgs.shape, val_labels.shape
+
+model = LitModel((3, 32, 32), dm.num_classes)
+
+# Initialize wandb logger
+wandb_logger = WandbLogger(project='wandb-lightning', job_type='train')
+
+# Initialize Callbacks
+early_stop_callback = pl.callbacks.EarlyStopping(monitor="val_loss")
+checkpoint_callback = pl.callbacks.ModelCheckpoint()
+
+# Initialize a trainer
+trainer = pl.Trainer(max_epochs=10,
+                     accelerator="gpu", 
+                     logger=wandb_logger,
+                     callbacks=[early_stop_callback,
+                                ImagePredictionLogger(val_samples),
+                                checkpoint_callback],
+                     )
+
+# Train the model ⚡🚅⚡
+trainer.fit(model, dm)
+
+# Evaluate the model on the held-out test set ⚡⚡
+trainer.test(dataloaders=dm.test_dataloader())
+
+# Close wandb run
+wandb.finish()
