3d62e4645d3ee9e0584d6c000cd36cf73ff39ea0
ShadowHammer <63354418+ShadowHammer@users.noreply.github.com>
2024-03-11 10:18:23 +0100

    Delete pothole_images directory

--- pothole_images/validation/ting.py
+++
@@ -1,8 +0,0 @@
-import os, csv
-
-f=open("C:/Users/tobia/Documents/Github/Bachelor/pothole_images/validation/validation.csv",'r+')
-w=csv.writer(f)
-for path, dirs, files in os.walk("C:/Users/tobia/Documents/Github/Bachelor/pothole_images/validation/images"):
-    for filename in files:
-        w.writerow([filename])
-
